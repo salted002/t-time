e622087295a3adbaf610ff3c51f7fcb0ae1e3f78
salted002 <salted002@gmail.com>
2026-09-22 12:24:08 +0900

    🗃️ db: config.js 파일에 Azure 접속용 SSL 옵션 추가

--- apps/backend/src/db/config/config.js
+++ apps/backend/src/db/config/config.js
@@ -11,23 +11,37 @@ module.exports = {
     define: {
       underscored: true,
     },
+    dialectOptions:
+      process.env.DB_HOST && process.env.DB_HOST.includes('azure')
+        ? { ssl: { require: true, rejectUnauthorized: false } }
+        : {},
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + '_test',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
     define: {
       underscored: true,
     },
+    dialectOptions:
+      process.env.DB_HOST && process.env.DB_HOST.includes('azure')
+        ? { ssl: { require: true, rejectUnauthorized: false } }
+        : {},
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     define: {
       underscored: true,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 }
